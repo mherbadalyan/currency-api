fc5469d8712e51be3063d89a2b805000e101a2aa
Mher Badalyan <mherbadalyan@gmail.com>
2024-03-22 12:28:56 +0400

    Initial commit

--- src/main/java/com/org/currency_api/application/dto/ExchangeRateDto.java
+++ src/main/java/com/org/currency_api/application/dto/ExchangeRateDto.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -14,4 +15,17 @@ public class ExchangeRateDto {
     private String currency;
     private LocalDateTime updateDate;
     private Map<String, Double> rates;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ExchangeRateDto that = (ExchangeRateDto) o;
+        return Objects.equals(currency, that.currency) && Objects.equals(updateDate, that.updateDate) && Objects.equals(rates, that.rates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, updateDate, rates);
+    }
 }
